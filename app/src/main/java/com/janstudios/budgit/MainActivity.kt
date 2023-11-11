ee01f6951dc8b492e8fe4a7a79d77d1d704d8bd5
jan furio <janfurio@tutanota.com>
2023-11-12 00:24:02 +0100

    Added more core functionalities

--- app/src/main/java/com/janstudios/budgit/MainActivity.kt
+++ app/src/main/java/com/janstudios/budgit/MainActivity.kt
@@ -2,10 +2,39 @@ package com.janstudios.budgit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var transactions : ArrayList<Transaction>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        transactions = arrayListOf(
+            Transaction("Pay", 400.00))
+
+
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    // Handle home navigation
+                    true
+                }
+                R.id.nav_savings -> {
+                    // Handle favorites navigation
+                    true
+                }
+                R.id.nav_wallet -> {
+                    // Handle search navigation
+                    true
+                }
+                else -> false
+            }
+        }
     }
+
+
+
 }
---
+++ app/src/main/java/com/janstudios/budgit/Transaction.kt
@@ -0,0 +1,4 @@
+package com.janstudios.budgit
+
+data class Transaction(val label: String, val amount: Double) {
+}
